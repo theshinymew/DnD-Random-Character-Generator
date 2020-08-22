ab20e89cf64e1b84ada446d9cf521bc74aa36632
zenith110 <thequake@live.com>
2020-08-22 16:06:56 -0400

    Finished part 1 of the html

--- app.py
+++ app.py
@@ -3,18 +3,16 @@ import requests
 app = Flask(__name__, static_url_path='/static')
 @app.route("/")
 def index():
-    return render_template("index.html", pstyleColor = "#F3EFE0", style = "display:none")
+    return render_template("index.html")
 @app.route("/character/", methods = ["POST"])
 def character():
     class_and_level = "Mage"
     class_background = ""
-    player_name = "Mudkip_warrior_69"
     race = "Mudkip"
     alignment = ""
     return render_template("character.html", 
     class_and_level = class_and_level,
     class_background = class_background, 
-    player_name = player_name,
     race = race,
     alignment = alignment)
 
