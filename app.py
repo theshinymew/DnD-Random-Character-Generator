6ffc5d0acbe564993c1be893bba287eed90cc1cc
zenith110 <thequake@live.com>
2020-08-22 22:53:34 -0400

    Parsed the characterdata.json

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 from flask import Flask, render_template, request, redirect
 import requests
+import json
+import random
 app = Flask(__name__, static_url_path='/static')
 @app.route("/")
 def index():
     return render_template("index.html")
 @app.route("/character/", methods = ["POST"])
 def character():
-    class_and_level = "Mage"
-    class_background = ""
-    race = "Mudkip"
-    alignment = ""
-    return render_template("character.html", 
-    class_and_level = class_and_level,
-    class_background = class_background, 
-    race = race,
-    alignment = alignment)
+    with open("static/CharacterData.json") as loop:
+        character = json.load(loop)
+
+    player_race = random.choice(character["playerRaces"])
+    player_class = random.choice(character["playerClasses"])
+    return render_template("character.html", race = player_race, class_and_level = (player_class + " 1"))
 
 if __name__ == '__main__':
     app.run
